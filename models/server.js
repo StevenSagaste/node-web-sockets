076288703de8b1606ed4ec7c5d3d1037d7d4d15d
Steven Sagaste <steven3140c@outlook.com>
2022-10-27 17:57:59 -0600

    primer commit

---
+++ models/server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const cors = require('cors');
+
+const { socketController } = require('../sockets/controller');
+
+class Server {
+
+    constructor() {
+        this.app    = express();
+        this.port   = process.env.PORT;
+        this.server = require('http').createServer( this.app );
+        this.io     = require('socket.io')( this.server );
+
+        this.paths = {
+        }
+
+
+        // Conectar a base de datos
+        // this.conectarDB();
+
+        // Middlewares
+        this.middlewares();
+
+        // Rutas de mi aplicación
+        this.routes();
+
+        this.sockets();
+    }
+
+    // async conectarDB() {
+    //     await dbConnection();
+    // }
+
+
+    middlewares() {
+
+        // CORS
+        this.app.use( cors() );
+
+        // Lectura y parseo del body
+        this.app.use( express.json() );
+
+        // Directorio Público
+        this.app.use( express.static('public') );
+
+    }
+
+    routes() {
+        
+        // this.app.use( this.paths.auth, require('../routes/auth'));
+        
+    }
+
+    sockets() {
+
+        this.io.on('connection', socketController );
+    }
+
+    listen() {
+        this.server.listen( this.port, () => {
+            console.log(`Servidor corriendo en http://localhost:${this.port}` );
+        });
+    }
+
+}
+
+
+
+
+module.exports = Server;
